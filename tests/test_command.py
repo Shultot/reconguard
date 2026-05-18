96d703ecfc32d1b670e3aaeaf0fa5fecf8923bb2
GuyMason1 <guymason2004@gmail.com>
2026-05-17 23:09:48 -0700

    Fix test command construction order

--- tests/test_command.py
+++ tests/test_command.py
@@ -4,19 +4,19 @@ from main import nmap_command
 @pytest.mark.parametrize("valid_ip, expected_outcome", [
     (
         "192.168.1.1",
-        ["nmap", "-sV", "-oX", "scan.xml", "192.168.1.1"]
+        ["nmap", "-sV", "192.168.1.1", "-oX", "scan.xml"]
     ),
     (
         "172.16.0.1",
-        ["nmap", "-sV", "-oX", "scan.xml", "172.16.0.1"]
+        ["nmap", "-sV", "172.16.0.1", "-oX", "scan.xml"]
     ),
     (
         "::1",
-        ["nmap", "-6", "-sV", "-oX", "scan.xml", "::1"]
+        ["nmap", "-6", "-sV", "::1", "-oX", "scan.xml"]
     ),
     (
         "fe80::1",
-        ["nmap", "-6", "-sV", "-oX", "scan.xml", "fe80::1"]
+        ["nmap", "-6", "-sV", "fe80::1", "-oX", "scan.xml"]
     )
 ])
 
